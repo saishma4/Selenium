21d59fb0e569db3ebcea229fce7d6eab5c0357e5
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-27 12:00:14 +0530

    page loading strategy commit

---
+++ src/test/java/test/PageLoadingDemo.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PageLoadingDemo {
+
+	public static void main(String[] args) {
+		
+		ChromeOptions options = new ChromeOptions();
+		
+		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+		options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+		options.setPageLoadStrategy(PageLoadStrategy.NONE);
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://google.com");
+		driver.quit();
+		
+
+	}
+
+}
