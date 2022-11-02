780d3571580c999525f4af5e43d02f4fcb4bd59a
saishma4 <chanda.saishma@ivycomptech.com>
2022-11-03 00:17:20 +0530

    testng commit

---
+++ src/test/java/test/GoogleSearchTestNG.java
@@ -0,0 +1,49 @@
+package test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.GoogleSearchPage;
+
+public class GoogleSearchTestNG {
+	
+	static WebDriver driver = null;
+	
+	@BeforeTest
+	public void setUpTest() {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public  void googleSearch() {
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://google.com");
+		
+		GoogleSearchPage.textBoxSearch(driver).sendKeys("Selenium");
+		GoogleSearchPage.buttonSearch(driver).sendKeys(Keys.RETURN);
+		
+	}
+	
+	@AfterTest
+	public void tearDownTest() {
+		
+		driver.close();
+		driver.quit();
+		System.out.println("Test completed");
+		
+	}
+
+}
